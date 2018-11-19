256d8fd78a16daa588df65a67953b377f25a480a
pedromoreira-estg <pmoreira.estg@gmail.com>
2018-11-19 11:00:47 +0000

    forms

---
+++ FORMS/dump.php
@@ -0,0 +1,11 @@
+<?php
+echo "<hr/>";
+echo "<h1>GET</h1>";
+var_dump($_GET);
+echo "<hr/>";
+echo "<h1>POST</h1>";
+var_dump($_POST);
+echo "<hr/>";
+echo "<h1>REQUEST</h1>";
+var_dump($_REQUEST);
+?>
